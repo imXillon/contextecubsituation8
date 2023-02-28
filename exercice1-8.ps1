85b87e1edcfe5aa13666838cdb8c7417ea65c387
unknown <Administrateur@LOCAL.EDIMBOURG.CUB.SIOPLC.FR>
2023-02-28 11:56:06 +0100

    exercice1-2.ps1

---
+++ exercice1-8.ps1
@@ -0,0 +1,16 @@
+﻿# Demande à l'utilisateur s'il souhaite la list des services démarrés ou arrêtés
+$etat = Read-Host "Souhaitez-vous la liste des services démarrés ou arrêtés (D/A)"
+#Créer des conditions en fonction du choix de l'utilisateur
+if ($etat -eq "D") {
+     $services = Get-Service | Where-Object {$_.status -like "running"} | Select-Object Name,DisplayName,Description,Status
+}
+elseif ($etat -eq "A") {
+     $services = Get-Service | Where-Object {$_.status -like "stopped"} | Select-Object Name,DisplayName,Description,Status
+}
+
+    
+
+
+# Exporter dans un fichier csv
+$services | Export-Csv -Path "C:\sauvegarde\services.csv"
+$services | Format-Table -AutoSize
---
+++ exercice1-9.ps1
@@ -0,0 +1,10 @@
+﻿# Importer les services démarrés depuis le fichier csv
+$services = Import-Csv -Path "C:\sauvegarde\services.csv" | Where-Object {$_.Status -eq "Running"}
+
+# Parcourir chaque service et afficher son nom et statut s'il contient le mot "system"
+foreach ($service in $services) {
+    if ($service.Name -like "*system*") {
+        Write-Host $service.Name -ForegroundColor Green -NoNewline
+
+    }
+}
